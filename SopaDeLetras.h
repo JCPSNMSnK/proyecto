fa1bbe495767d7197e0908c6632048a8f079b917
JCPSNMSnK <112967118+JCPSNMSnK@users.noreply.github.com>
2022-11-14 19:08:19 -0300

    Update SopaDeLetras.h

--- SopaDeLetras.h
+++ SopaDeLetras.h
@@ -5,6 +5,16 @@
 typedef char tString[50];
 typedef tString tVectorStrings[MAX];
 
+typedef struct {
+  tString nombreJugador;
+  int puntaje;
+} tDato;
+
+typedef struct{
+  tDato infoJugador;
+  struct nodo * siguiente;
+}tListaPuntajes;
+
 void sopaDeLetras();
 void mostrarSopa();
 void aciertoOError(tString );
@@ -12,5 +22,12 @@ void inicializarScore();
 bool buscarPalabraSecuencial(tVectorStrings , tString );
 void actualizarScore ();
 void instrucciones();
+
+void inicializarLista();
+bool listaVacia(tListaPuntajes * );
+void insertarPrimerPuntaje (tString , int );
+void insertarPuntajeAdelante(tString, int );
+void insertarPuntaje(tString , int );
+
 
 #endif
