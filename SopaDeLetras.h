95529d1a8e8b6592111237412d221525105286e9
JCPSNMSnK <112967118+JCPSNMSnK@users.noreply.github.com>
2022-11-14 20:28:20 -0300

    Update SopaDeLetras.h

--- SopaDeLetras.h
+++ SopaDeLetras.h
@@ -15,6 +15,12 @@ typedef struct{
   struct nodo * siguiente;
 }tListaPuntajes;
 
+typedef struct nodoArbol {
+  tDato datoArPunt;
+  struct nodoArbol *hijoIzq;
+  struct nodoArbol *hijoDer;
+}tArbolPuntaje;
+
 void sopaDeLetras();
 void mostrarSopa();
 void aciertoOError(tString );
@@ -23,15 +29,23 @@ bool buscarPalabraSecuencial(tVectorStrings , tString );
 void actualizarScore ();
 void instrucciones();
 
+//Lista Enlazada como Pila
 void inicializarLista();
 bool listaVacia(tListaPuntajes * );
 void insertarPrimerPuntaje (tString , int );
 void insertarPuntajeAdelante(tString, int );
 void insertarPuntaje(tString , int );
 
+//Archivos
 void generarRegistro();
 void iniciaProceso();
 void grabarRegistroPuntaje(tListaPuntajes * );
 void finalizarProceso();
+
+//Arbol
+void crearArbolVacio (tArbolPuntaje * );
+bool arbolVacio(tArbolPuntaje * );
+void insertarPuntajeArbol (tArbolPuntaje ** , tDato);
+void recorrrerInOrden(tArbolPuntaje * );
 
 #endif
